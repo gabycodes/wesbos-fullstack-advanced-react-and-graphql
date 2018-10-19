a3c111aae94ecd31afb8036b210c9e342b3a778c
Gaby Küpfer <gaby@itsme3d.com>
2018-10-19 11:22:06 -0400

    First commit

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,23 @@
-const mutations = {};
+const Mutation = {
+  async createItem(parents, args, context, info) {
+    // TODO: Check if they are logged in
 
-module.exports = mutations;
+    const item = await context.db.mutation.createItem({
+      data: {
+        ... args
+      }
+    }, info)
+
+    console.log(item);
+    return item;
+  }
+  // createDog(parent, args, context, info) {
+  //   global.dogs = global.dogs || [];
+  //   // create a dog!
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+//   }
+};
+
+module.exports = Mutation;
