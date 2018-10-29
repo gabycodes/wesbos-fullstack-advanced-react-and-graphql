5b8911d70527ac381d183b354780d060eea33db9
Gaby Küpfer <gaby@itsme3d.com>
2018-10-29 16:16:51 -0400

    Finished video 19. Can update items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -9,14 +9,23 @@ const Mutation = {
     }, info) // info contains the query. Our mutation needs access to the query so it can specify what data gets returned from the DB
 
     return item;
-  }
-  // createDog(parent, args, context, info) {
-  //   global.dogs = global.dogs || [];
-  //   // create a dog!
-  //   const newDog = { name: args.name };
-  //   global.dogs.push(newDog);
-  //   return newDog;
-//   }
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutation;
